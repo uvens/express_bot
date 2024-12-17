dd9421d820f096794e7ac6baa06d22220b25ae8b
uventus <uventus_work@mail.ru>
2024-12-17 15:33:37 +0300

    fixed : port

--- main.py
+++ main.py
@@ -96,4 +96,4 @@ async def callback_handler(request: Request) -> JSONResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9123)
