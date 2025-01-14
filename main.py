09a530dbdafd14e246b05a864ab97cfa6eff41c9
uventus <uventus_work@mail.ru>
2025-01-14 11:31:41 +0300

    fixed : fixed status

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ async def callback_handler(request: Request) -> JSONResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8082)
